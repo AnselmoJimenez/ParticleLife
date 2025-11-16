b8a835ab516a3fd8c7b49fd07dfcf802f0cde44e
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-11-15 23:32:56 -0500

    testing

--- source/main.c
+++ source/main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 
 #include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#include "styles/style_dark.h"
+
 #include "../include/interface.h"
 
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "Particle Life");
-    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+    GuiLoadStyleDark(); // Load Dark Style
+
+    Font font = LoadFont("./assets/fonts/OpenSans.ttf");
 
     // Main game loop
     while (!WindowShouldClose()) {
         // Draw
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground((Color) {20, 20, 18, 255});
 
         // GUI Controls
-        init_interface();
+        init_interface(font);
 
         EndDrawing();
     }
